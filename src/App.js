3b18e9a78b4c15d0cdea2bf8ff9d2c100e8ebdfa
Lucas Reis <lucasmreis@gmail.com>
2017-07-14 23:00:21 -0300

    styled components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,59 @@
-import React, { Component } from 'react';
-import MenuItem from './components/MenuItem';
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import MenuItem from './components/MenuItem'
+
+const ContentsContainer = styled.div`
+  color: white;
+  position: absolute;
+  top: 0;
+  left: 0;
+  padding: 1em;
+  opacity: 1;
+  transition: all .2s ease-in 0s;
+
+  & a {
+    text-decoration: underline;
+    cursor: pointer;
+  }
+`
 
 const contents = name => onClose => (
-  <div>
+  <ContentsContainer>
     <h2>Contents of {name}</h2>
     <a onClick={onClose}>Back</a>
-  </div>
+  </ContentsContainer>
 )
 
-const isSelected = (i, { selected }) => !selected || selected === i
+const color = {
+  A: '#1abc9c',
+  B: '#f1c40f',
+  C: '#e74c3c',
+  D: '#3498db',
+  E: '#e67e22',
+  F: '#2ecc71',
+  G: '#9b59b6'
+}
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 100vh;
+  font-size: 32px;
+  font-weight: 800;
+  perspective: 600px;
+  transform: ${props => props.selected ? 'skew(0)' : 'skew(-10deg)'};
+  transition: all .2s ease-in-out;
+  position: relative;
+`
+
+const isSelected = (i, { selected }) => selected === i
+
+const allClosed = ({ selected }) => selected === null
 
 class App extends Component {
   state = {
-    items: ['A', 'B', 'C', 'D', 'E', 'F', 'G'],
+    items: ['A', 'B', 'C', 'D', 'E'],
     selected: null
   }
 
@@ -22,19 +63,21 @@ class App extends Component {
 
   render() {
     return (
-      <div className={`container ${this.state.selected ? 'container-selected' : ''}`}>
+      <Container selected={this.state.selected}>
         {this.state.items.map(i => (
           <MenuItem
             key={i}
             name={i}
+            color={color[i]}
+            allClosed={allClosed(this.state)}
             selected={isSelected(i, this.state)}
             onSelectItem={() => this.select(i)}
             onClose={this.close}>
             {contents(i)}
           </MenuItem>
         ))}
-      </div>
-    );
+      </Container>
+    )
   }
 }
 
--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -1,10 +1,80 @@
-import React from 'react';
+import React from 'react'
+import styled from 'styled-components'
 
-export default ({ name, selected = false, children, onSelectItem, onClose }) => (
-  <div className={`item ${name} ${selected ? 'item-selected' : 'item-not-selected'}`}>
-    <a onClick={onSelectItem} className="item-link">
-      {name}
-    </a>
-    {children(onClose)}
-  </div>
+const Item = styled.div`
+  cursor: ${props => props.selected ? 'unset' : 'pointer'};
+  width: ${props => props.allClosed || props.selected ? '100' : '2'}%;
+  height: 100vh;
+  opacity: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  background: ${props => props.color};
+  transition: opacity .2s ease-in-out 0.2s, transform .2s ease-in-out, width .2s ease-in-out .1s;
+
+  &:last-child::after {
+    content: '';
+    width: 100vw;
+    background: ${props => props.color};
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    transform: translateX(99%);
+  }
+
+  &:first-child::before {
+    content: '';
+    width: 100vw;
+    background: ${props => props.color};
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    transform: translateX(-99%);
+  }
+`
+
+const ItemLink = styled.a`
+  text-decoration: none;
+  opacity: ${props => !props.allClosed || props.selected ? '0' : '1'};
+  transform: skew(${props => props.allClosed ? '10' : '0'}deg);
+  transition: all .2s ease-in ${props => props.allClosed ? '.2' : '0'}s;
+  color: white;
+  cursor: pointer;
+`
+
+const FadeInDiv = styled.div`
+  opacity: ${props => props.opacity};
+  transition: all .2s ease-in-out .2s;
+`
+
+class FadeIn extends React.Component {
+  state = { opacity: 0 }
+  componentDidMount() { setTimeout(() => this.setState({ opacity: 1 })) }
+  render() {
+    return (
+      <FadeInDiv opacity={this.state.opacity}>
+        {this.props.children}
+      </FadeInDiv>
+    )
+  }
+}
+
+export default ({ name, color, selected = false, allClosed = true, children, onSelectItem, onClose }) => (
+  <Item
+    selected={selected}
+    allClosed={allClosed}
+    color={color}
+    onClick={() => selected ? () => {} : onSelectItem()}>
+    {selected
+      ? <FadeIn>{children(onClose)}</FadeIn>
+      : (
+          <ItemLink onClick={onSelectItem} selected={selected} allClosed={allClosed}>
+            {name}
+          </ItemLink>
+      )
+    }
+  </Item>
 )

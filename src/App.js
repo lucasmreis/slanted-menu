11256d242dada4ffc5bd89644ffc1c566d5590fa
Lucas Reis <lucasmreis@gmail.com>
2017-07-15 00:03:59 -0300

    fix lint

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import styled from 'styled-components'
 import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
 import MenuItem from './components/MenuItem'
